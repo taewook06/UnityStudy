84cd13baaafde8139ba2da27705e6c1c8438e0c3
taewook06 <ptw061120@naver.com>
2022-09-02 00:10:20 +0900

    123123

--- First2DProject/Assets/Chapter3/CarController.cs
+++ First2DProject/Assets/Chapter3/CarController.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CarController : MonoBehaviour
 {
     float speed = 0;
     Vector2 startPos;
-
+    bool OverText = false;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -17,28 +19,51 @@ public class CarController : MonoBehaviour
     void Update()
     {
 
-
-        //마우스 클릭했을 때
-        if (Input.GetMouseButtonDown(0))
+        if (GameObject.Find("roulette").GetComponent<RouletteController>().carMoveChance > 0)
         {
-            //마우스 위치좌표 저장
-            startPos = Input.mousePosition;
+            while(GameObject.Find("roulette").GetComponent<RouletteController>().carMoveChance > 0)
+            {
+                //마우스 클릭했을 때
+                if (Input.GetMouseButtonDown(0))
+                {
+                    //마우스 위치좌표 저장
+                    startPos = Input.mousePosition;
+                }
+
+                //마우스클릭에서 땠을 때
+                else if (Input.GetMouseButtonUp(0))
+                {
+                    Vector2 endPos = Input.mousePosition;
+                    float swipeLiength = endPos.x - startPos.x;
+
+
+                    speed = swipeLiength / 500.0f;
+
+                    GetComponent<AudioSource>().Play();
+
+                    Reduction();
+                }
+
+                transform.Translate(speed, 0, 0);
+
+                speed *= 0.98f;
+                
+            }
+            OverText = true;           
         }
-
-        //마우스클릭에서 땠을 때
-        else if (Input.GetMouseButtonUp(0))
+        else if (OverText == true)
         {
-            Vector2 endPos = Input.mousePosition;
-            float swipeLiength = endPos.x - startPos.x;
-
-
-            speed = swipeLiength / 500.0f;
-
-            GetComponent<AudioSource>().Play();
-        }       
-
-        transform.Translate(speed, 0, 0);
-
-        speed *= 0.98f;
+            if(GameObject.Find("roulette").GetComponent<RouletteController>().carMoveChance == 0)
+            {
+                GameObject.Find("Chance").GetComponent<Text>().text = "기회를 모두 사용하였습니다.";
+                OverText = false;
+            }
+           
+        }          
     }
+    static void Reduction()
+    {
+        GameObject.Find("roulette").GetComponent<RouletteController>().carMoveChance -= GameObject.Find("roulette").GetComponent<RouletteController>().carMoveChance;
+    }
+   
 }
--- First2DProject/Assets/Chapter3/RouletteController.cs
+++ First2DProject/Assets/Chapter3/RouletteController.cs
@@ -5,10 +5,11 @@ using UnityEngine.UI;
 
 public class RouletteController : MonoBehaviour
 {
-    GameObject Chance;
+    public GameObject Chance;
     float rotSpeed = 0; // 회전 속도
     bool end = false;
-    int carMoveChance;
+    public int carMoveChance;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -19,62 +20,68 @@ public class RouletteController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //클릭하는 동안 회전한다.
-        if (Input.GetMouseButton(0))
+
+        gameObject.GetComponent<RouletteController>().enabled = true;
         {
-            rotSpeed = 10;
-            end = true;
-        }
-        //회전 속도를 줄인다.
-        rotSpeed *= 0.96f;
-
-        //회전 속도만큼 룰렛을 회전시킨다. 
-        gameObject.transform.Rotate(0, 0, rotSpeed);
-
-        //출력
-        if (rotSpeed <= 0.01)
-        {
-
-            if (end == true)
+            //클릭하는 동안 회전한다.
+            if (Input.GetMouseButton(0))
             {
-                if (gameObject.transform.eulerAngles.z > 330 || gameObject.transform.eulerAngles.z <= 30)
+                rotSpeed = 10;
+                end = true;
+            }
+            //회전 속도를 줄인다.
+            rotSpeed *= 0.96f;
+
+            //회전 속도만큼 룰렛을 회전시킨다. 
+            gameObject.transform.Rotate(0, 0, rotSpeed);
+
+            //출력
+            if (rotSpeed <= 0.01)
+            {
+
+                if (end == true)
                 {
-                    Debug.Log("운수 나쁨");
-                    carMoveChance = 1;
-                    Chance.GetComponent<Text>().text = $"뽑은 기회: {carMoveChance}";
+                    if (gameObject.transform.eulerAngles.z > 330 || gameObject.transform.eulerAngles.z <= 30)
+                    {
+                        Debug.Log("운수 나쁨");
+                        carMoveChance = 1;
+                        Chance.GetComponent<Text>().text = $"뽑은 기회: {carMoveChance}";
+                    }
+                    else if (gameObject.transform.eulerAngles.z > 30 && gameObject.transform.eulerAngles.z <= 90)
+                    {
+                        Debug.Log("운수 대통");
+                        carMoveChance = 2;
+                        Chance.GetComponent<Text>().text = $"뽑은 기회: {carMoveChance}";
+                    }
+                    else if (gameObject.transform.eulerAngles.z > 90 && gameObject.transform.eulerAngles.z <= 150)
+                    {
+                        Debug.Log("운수 매우 나쁨");
+                        carMoveChance = 1;
+                        Chance.GetComponent<Text>().text = $"뽑은 기회: {carMoveChance}";
+                    }
+                    else if (gameObject.transform.eulerAngles.z > 150 && gameObject.transform.eulerAngles.z <= 210)
+                    {
+                        Debug.Log("운수 보통");
+                        carMoveChance = 3;
+                        Chance.GetComponent<Text>().text = $"뽑은 기회: {carMoveChance}";
+                    }
+                    else if (gameObject.transform.eulerAngles.z > 210 && gameObject.transform.eulerAngles.z <= 270)
+                    {
+                        Debug.Log("운수 조심");
+                        carMoveChance = 2;
+                        Chance.GetComponent<Text>().text = $"뽑은 기회: {carMoveChance}";
+                    }
+                    else
+                    {
+                        Debug.Log("운수 좋음");
+                        carMoveChance = 4;
+                        Chance.GetComponent<Text>().text = $"뽑은 기회: {carMoveChance}";
+                    }
+                    end = false;
+
+                    gameObject.GetComponent<RouletteController>().enabled = false;
                 }
-                else if (gameObject.transform.eulerAngles.z > 30 && gameObject.transform.eulerAngles.z <= 90)
-                {
-                    Debug.Log("운수 대통");
-                    carMoveChance = 2;
-                    Chance.GetComponent<Text>().text = $"뽑은 기회: {carMoveChance}";
-                }
-                else if (gameObject.transform.eulerAngles.z > 90 && gameObject.transform.eulerAngles.z <= 150)
-                {
-                    Debug.Log("운수 매우 나쁨");
-                    carMoveChance = 1;
-                    Chance.GetComponent<Text>().text = $"뽑은 기회: {carMoveChance}";
-                }
-                else if (gameObject.transform.eulerAngles.z > 150 && gameObject.transform.eulerAngles.z <= 210)
-                {
-                    Debug.Log("운수 보통");
-                    carMoveChance = 3;
-                    Chance.GetComponent<Text>().text = $"뽑은 기회: {carMoveChance}";
-                }
-                else if (gameObject.transform.eulerAngles.z > 210 && gameObject.transform.eulerAngles.z <= 270)
-                {
-                    Debug.Log("운수 조심");
-                    carMoveChance = 2;
-                    Chance.GetComponent<Text>().text = $"뽑은 기회: {carMoveChance}";
-                }
-                else
-                {
-                    Debug.Log("운수 좋음");
-                    carMoveChance = 4;
-                    Chance.GetComponent<Text>().text = $"뽑은 기회: {carMoveChance}";
-                }
-                end = false;
             }
         }
-    }
+    }  
 }
